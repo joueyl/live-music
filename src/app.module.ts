8d04df3eac1176bbf38e6b6cc5a57a29a9644c69
joueyl <dzb08042@163.com>
2024-09-06 17:16:32 +0800

    clear:清理初始代码

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,31 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { ConfigModule,ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
-import { MailModule } from './mail/mail.module';
+import { ConfigModule } from '@nestjs/config';
+import { MongodbModule } from './mongodb/mongodb.module';
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       envFilePath: '.env',
     }),
-    MongooseModule.forRootAsync(
-      {
-        imports:[ConfigModule],
-        inject:[ConfigService],  
-        useFactory:(configService:ConfigService)=>{
-          return {
-          uri:`mongodb://${configService.get("DATABASE_HOST")}:${configService.get("DATABASE_PORT")}`,
-          user:configService.get("DATABASE_USER_NAME"),
-          dbName:configService.get("DATABASE_NAME"),
-          pass:configService.get('DATABASE_PASSWORD')
-          }
-        },
-      }
-    ),
-    UserModule,
-    MailModule,
+    UserModule
   ],
   controllers: [AppController],
   providers: [AppService],
