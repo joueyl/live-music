4300c4a49522bf85ddad8c1ff7656d4e2550d7fa
joueyl <dzb08042@163.com>
2024-09-12 17:30:36 +0800

    打通WebRTC

--- src/webrtc/webrtc.gateway.ts
+++ src/webrtc/webrtc.gateway.ts
@@ -5,33 +5,37 @@ import {
   MessageBody,
   ConnectedSocket,
   OnGatewayConnection,
+  OnGatewayDisconnect
 } from '@nestjs/websockets';
 import {WebrtcService} from './webwtc.service'
 import { Server, Socket } from 'socket.io';
 @WebSocketGateway({ cors: true })
-export class WebrtcGateway implements OnGatewayConnection {
+export class WebrtcGateway implements OnGatewayConnection,OnGatewayDisconnect {
   @WebSocketServer()
   server: Server;
 
   constructor(private readonly webrtc:WebrtcService) {}
 
   handleConnection(client: Socket) {
-    console.log(client.handshake.auth);
-    console.log('客户端已连接');
-    client.emit('connectionSuccess');
+    console.clear()
+    console.log(`用户:${client.handshake.auth.user_name} 已连接信令服务器`);
+    if(this.webrtc.peer.destroyed){
+      this.webrtc.peer.destroy()
+      this.webrtc.createPeer()
+      this.webrtc.peer.on('connect',()=>{
+        this.webrtc.pushAudioStream()
+        console.log(`用户: ${client.handshake.auth.user_name} 连接webrtc成功`);
+      })
+    }
   }
-
+  handleDisconnect(client: Socket) {
+    console.log(`用户:${client.handshake.auth.user_name} 断开连接`);
+  }
   @SubscribeMessage('offer')
-  async sendAnser(@MessageBody() data: { sdp: any }, @ConnectedSocket() client: Socket) {
-    console.log('接收到offer');
-      this.webrtc.peer.signal(data.sdp)
+  async sendAnser(@MessageBody() data, @ConnectedSocket() client: Socket) {
+      this.webrtc.peer.signal(data)
       this.webrtc.peer.on('signal',(answer)=>{
         client.emit('answer',answer)
       })
-  }
-  @SubscribeMessage('candidate')
-  handleIceCandidate(@MessageBody() candidate: any) {
-    console.log('交换ice');
-    this.webrtc.peer.signal(candidate);
   }
 }
